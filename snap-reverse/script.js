32571462638bd48ee56cd69b4a4bb32fefc9c56a
Adian Tsosie <addve12@yahoo.com>
2019-02-21 08:45:45 -0700

    Snap challenge reverse by reduce method.

--- snap-reverse/script.js
+++ snap-reverse/script.js
@@ -1,3 +1,5 @@
 const str = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human" +
-    "loss as keenly as we feel one of those close to us, human history would be far less bloody."
-        [str].reduce((prev,next)=>next+prev)
+    "loss as keenly as we feel one of those close to us, human history would be far less bloody.";
+        [...str].reduce((prev,next)=>next+prev);
+
+console.log(str.reduce(reducer));
